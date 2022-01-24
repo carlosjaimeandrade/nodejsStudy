58568eb9ea6fdeffe64ec8be9d814f9af0d9f0a5
Carlos jaime <jaime_andrek@hotmail.com>
2022-01-24 17:43:05 -0300

    modulo exportando varias funçõe

--- modulos.js
+++ modulos.js
@@ -1,17 +1,5 @@
-var somaFuncao = require('./calculadora')
+var calculadora = require('./calculadora')
 
-function mult(a, b) {
-    return a + b;
-}
-
-function sub(a, b) {
-    return a - b
-}
-
-function div(a, b) {
-    return a / b
-}
-
-const a = somaFuncao(8, 22)
+const a = calculadora.soma(8, 22)
 
 console.log(a)
