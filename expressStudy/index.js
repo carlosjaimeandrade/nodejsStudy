dd34aad62a579a9cdce4e466e260794ee7f3d1d3
Carlos jaime <jaime_andrek@hotmail.com>
2022-01-24 20:04:18 -0300

    Parametros opcionais ROTAS

--- expressStudy/index.js
+++ expressStudy/index.js
@@ -6,8 +6,11 @@ app.get('/', function(req, res) {
     res.send("ola mundo") //enviando a resposta os codigos abaixo não serão lidos
 })
 
-app.get('/blog', function(req, res) {
-    res.send("ola blog") //enviando a resposta os codigos abaixo não serão lidos
+//parametros não obrigatorios
+app.get('/blog/:artigo?', function(req, res) {
+    const artigo = req.params.artigo ? req.params.artigo : "";
+
+    res.send("ola blog " + artigo) //enviando a resposta os codigos abaixo não serão lidos
 })
 
 app.get('/canal/youtube', function(req, res) {
@@ -18,8 +21,9 @@ app.get('/ola/:nome/:empresa', function(req, res) {
     res.send(`oi ${req.params.nome} da empresa ${req.params.empresa}`) //enviando a resposta os codigos abaixo não serão lidos
 })
 
-
-
+app.get('/loja', function(req, res) {
+    res.send(`Os dados do parametros são ${req.query.id}`)
+})
 
 app.listen(4000, function(erro) {
     if (erro) {
